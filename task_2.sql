8a48dc82029082de4be79fea4b8592cb6af89b36
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-08-24 06:29:04 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
-    address VARCHAR(255)
+    address TEXT
 );
 
 -- =====================
@@ -60,6 +60,7 @@ CREATE TABLE IF NOT EXISTS Order_details (
     FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
 );
+
 
 
 
